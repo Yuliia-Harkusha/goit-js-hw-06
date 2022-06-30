c0ae8fbd621aa55339edbe18c6af7d3dad1b866e
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-06-30 13:37:22 +0200

    task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,8 @@
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +17,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector(".gallery");
+const item = document.createElement('li');
+
+const markupGallery = images.map(({url, alt}) => `<li><img alt = ${alt} src = ${url} width = 480px ></li>`).join('');
+
+list.insertAdjacentHTML("beforeend", markupGallery);
+
+item.classList.add("img");
+// console.log(item.classList);
+
+
