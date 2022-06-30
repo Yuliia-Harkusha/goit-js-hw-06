ed9ed1a20884f7909f1380d9af9d8c74f0657c41
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-06-30 15:16:19 +0200

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -2,5 +2,24 @@
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
 
-const counterValue = 0;
+const result = document.querySelector("#value");
 
+let counterValue = 0;
+
+const decrButton = document.querySelector('button[data-action="decrement"]');
+
+const incrButton = document.querySelector('button[data-action="increment"]');
+
+function decrement() {
+    counterValue -= 1;
+    result.textContent = counterValue;
+}
+
+function increment() {
+    counterValue += 1;
+    result.textContent = counterValue;
+}
+
+decrButton.addEventListener("click", decrement);
+
+incrButton.addEventListener("click", increment);
